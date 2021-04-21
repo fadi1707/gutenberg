b1a10bb38f37ef5edb54f11d6252a9720cc51d00
fadi <fadi1707@outlook.com>
2021-04-21 10:35:06 +0300

    serving static files from nginx

--- project/start_service..sh
+++ project/start_service..sh
@@ -6,4 +6,4 @@ chmod +x manage.py
 $_PYTHON manage.py migrate
 $_PYTHON manage.py updatecatalog 
 $_PYTHON manage.py collectstatic --noinput
-$_PYTHON manage.py runserver
+$_PYTHON manage.py runserver 0.0.0.0:8000
